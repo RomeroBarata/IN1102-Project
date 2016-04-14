7255abae344ff1a9e6e8911eab417e01242e77d2
RomeroBarata <romero.barata@gmail.com>
2016-04-14 17:24:08 -0300

    Rm old files

--- data_functions.R
+++
@@ -1,5 +0,0 @@
-readDataSets <- function(data_path, files_names){
-  lapply(files_names, function(file_name, data_path){
-    read.table(file = paste(data_path, file_name, sep = ""), header = FALSE)
-  }, data_path = data_path)
-}
