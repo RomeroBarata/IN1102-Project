e889c2aec995c034d737b0e097cfdd2c1659e9f3
RomeroBarata <romero.barata@gmail.com>
2016-04-17 07:55:39 -0300

    Function to add the class labels to each example

--- R/data_functions.R
+++ R/data_functions.R
@@ -3,3 +3,7 @@ readDataSets <- function(data_path, files_names){
         read.table(file.path(data_path, file_name), header = FALSE)
     }, data_path = data_path)
 }
+
+addClasses <- function(data_sets_list){
+    lapply(data_sets_list, transform, Class = rep(0:9, each = 200))
+}
