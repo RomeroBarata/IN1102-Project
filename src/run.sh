9558513f6227a7696829b446d2f59fe803806aa7
dppb05 <dppb@cin.ufpe.br>
2016-05-26 22:20:58 -0300

    Updated src/README and added T value in src/run.sh.

--- src/run.sh
+++ src/run.sh
@@ -5,9 +5,9 @@
 #   - the path containing the matrices to be used;
 
 DATA_PATH=$1
-# q=1*, k=10*, m=2*, epsilon=1e-10*, 4 instances, R data into
-# 'mvfcmv-data.R'
+# q=1*, k=10*, m=1.6*, epsilon=1e-10*, T=150*, 4 instances, R data
+# into 'mvfcmv-data.R'
 # * - fixed values for this project.
-PRG_ARGS="-q 1 -k 10 -m 2 -e 1e-10 -i 4 --Rfile mvfcmv-data.R"
+PRG_ARGS="-q 1 -k 10 -m 1.6 -e 1e-10 -i 4 -T 150 --Rfile mvfcmv-data.R"
 
 ./mvfcmv $PRG_ARGS 2000 3 $DATA_PATH/mfeat-fac-diss $DATA_PATH/mfeat-fou-diss $DATA_PATH/mfeat-kar-diss
