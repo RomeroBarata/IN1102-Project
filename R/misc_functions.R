71482b8b63563b983692aad78cafd6ea11cce952
RomeroBarata <romero.barata@gmail.com>
2016-06-11 18:56:19 -0300

    Brute force optimization functions + function to compute a confidence interval for the results

--- R/misc_functions.R
+++ R/misc_functions.R
@@ -5,3 +5,34 @@ majorityVote <- function(objects_list, newdata_list, ...){
         as.integer(names(table(row))[idx])
     })
 }
+
+bruteForceOptimSVM <- function(..., params){
+    mean_acc <- vector(mode = "numeric", length = nrow(params))
+    for (i in 1:nrow(params)){
+        svms_params <- list(list(C = params[i, 1]), 
+                            list(C = params[i, 2]), 
+                            list(C = params[i, 3]))
+        mean_acc[i] <- repeatedCVTrain(..., method_args = svms_params)$Accuracy_MEAN
+    }
+    cbind(params, mean_acc)
+}
+
+bruteForceOptimNN <- function(..., params){
+    mean_acc <- vector(mode = "numeric", length = nrow(params))
+    for (i in 1:nrow(params)){
+        nns_params <- list(list(size = params[i, 1], decay = params[i, 2], maxit = 1500), 
+                           list(size = params[i, 3], decay = params[i, 4], maxit = 1500), 
+                           list(size = params[i, 5], decay = params[i, 6], maxit = 1500))
+        mean_acc[i] <- repeatedCVTrain(..., method_args = nns_params)$Accuracy_MEAN
+    }
+    cbind(params, mean_acc)
+}
+
+createConfidenceInterval <- function(values, confidence = 0.99){
+    x <- mean(values)
+    s <- sd(values)
+    n <- length(values)
+    
+    error <- qt(confidence, df = n - 1) * s / sqrt(n)
+    c(error, x - error, x + error)
+}
